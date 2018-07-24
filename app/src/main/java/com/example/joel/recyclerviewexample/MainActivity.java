20866d08d5f38e46e5f8a9563857fc72acc6b06f
joelhenrique2000 <joelhenrique2000@gmail.com>
2018-07-23 21:51:26 -0300

    Configurando o recyclerView

--- app/src/main/java/com/example/joel/recyclerviewexample/MainActivity.java
+++ app/src/main/java/com/example/joel/recyclerviewexample/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.joel.recyclerviewexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.recyclerView = findViewById(R.id.recyclerView);
+
+        // Configurando o adapter
+
+
+        // Configurando RecyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext()); // Layout Manager
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true); // Tamanho fixo
+        //recyclerView.setAdapter();
+
     }
 }
